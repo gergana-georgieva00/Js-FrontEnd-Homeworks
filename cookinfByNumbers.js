8575bcdbc73eec8f30c715fcb168ad8405bcf954
Gergana Georgieva <gerganagergana1@gmail.com>
2023-11-13 17:46:29 +0200

    ex 12

---
+++ cookinfByNumbers.js
@@ -0,0 +1,116 @@
+function solve(num, op1, op2, op3, op4, op5) {
+    switch (op1) {
+        case "chop":
+            num /= 2;
+            console.log(num);
+            break;
+        case "dice":
+            num = Math.sqrt(num);
+            console.log(num);
+            break;
+        case "spice":
+            num += 1;
+            console.log(num);
+            break;
+        case "bake":
+            num *= 3;
+            console.log(num);
+            break;
+        case "fillet":
+            num -= num * 0.2;
+            console.log(num);
+            break;
+    }
+
+    switch (op2) {
+        case "chop":
+            num /= 2;
+            console.log(num);
+            break;
+        case "dice":
+            num = Math.sqrt(num);
+            console.log(num);
+            break;
+        case "spice":
+            num += 1;
+            console.log(num);
+            break;
+        case "bake":
+            num *= 3;
+            console.log(num);
+            break;
+        case "fillet":
+            num -= num * 0.2;
+            console.log(num);
+            break;
+    }
+
+    switch (op3) {
+        case "chop":
+            num /= 2;
+            console.log(num);
+            break;
+        case "dice":
+            num = Math.sqrt(num);
+            console.log(num);
+            break;
+        case "spice":
+            num += 1;
+            console.log(num);
+            break;
+        case "bake":
+            num *= 3;
+            console.log(num);
+            break;
+        case "fillet":
+            num -= num * 0.2;
+            console.log(num);
+            break;
+    }
+
+    switch (op4) {
+        case "chop":
+            num /= 2;
+            console.log(num);
+            break;
+        case "dice":
+            num = Math.sqrt(num);
+            console.log(num);
+            break;
+        case "spice":
+            num += 1;
+            console.log(num);
+            break;
+        case "bake":
+            num *= 3;
+            console.log(num);
+            break;
+        case "fillet":
+            num -= num * 0.2;
+            console.log(num);
+            break;
+    }
+
+    switch (op5) {
+        case "chop":
+            num /= 2;
+            console.log(num);
+            break;
+        case "dice":
+            num = Math.sqrt(num);
+            console.log(num);
+            break;
+        case "spice":
+            num += 1;
+            console.log(num);
+            break;
+        case "bake":
+            num *= 3;
+            console.log(num);
+            break;
+        case "fillet":
+            num -= num * 0.2;
+            console.log(num);
+            break;
+    }
+}
