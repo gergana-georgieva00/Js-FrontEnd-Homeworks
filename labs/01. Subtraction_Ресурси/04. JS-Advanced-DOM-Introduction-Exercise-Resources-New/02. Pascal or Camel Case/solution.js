03141cc7d254fff17e38f2d485fa287095cd7af1
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-14 19:58:49 +0200

    ex2

--- labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById('text').value;
+  const namingConvention = document.getElementById('naming-convention').value;
+  let result = '';
+
+  if (namingConvention === 'Camel Case') {
+    result = text.split(' ').map((word, index) =>
+      index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+  } else if (namingConvention === 'Pascal Case') {
+    result = text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+  } else {
+    result = 'Error!'
+  }
+
+  document.getElementById('result').innerText = result;
 }
