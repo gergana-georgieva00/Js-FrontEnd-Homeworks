a09dff7da0f90cd5ac2deec4c8a250eb527e7295
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-14 20:55:01 +0200

    ex4

--- labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   const items = document.querySelectorAll('#towns li');
+   const searchedItem = document.getElementById('searchText').value;
+   let matches = 0;
+
+   for (let index = 0; index < items.length; index++) {
+      const element = items[index];
+      if(element.textContent.includes(searchedItem)){
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+         matches++;
+      }
+   }
+
+   const textMessage = `${matches} matches found`;
+   document.getElementById('result').textContent = textMessage;
 }
