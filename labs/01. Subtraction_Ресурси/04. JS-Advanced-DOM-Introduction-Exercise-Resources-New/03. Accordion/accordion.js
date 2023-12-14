a234a0adfc131bd8ae08b453ce8e1e100db2f792
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-14 20:23:15 +0200

    ex3

--- labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,8 @@
 function toggle() {
-    console.log('TODO:...');
+    //document.getElementById('extra').style.display = 'inline';
+    //document.getElementsByClassName('button')[0].textContent = 'Less'
+
+    document.getElementsByClassName('button')[0].textContent === 'More' ? 
+    (document.getElementsByClassName('button')[0].textContent = 'Less', document.getElementById('extra').style.display = 'block') 
+    : (document.getElementsByClassName('button')[0].textContent = 'More', document.getElementById('extra').style.display = 'none')
 }
