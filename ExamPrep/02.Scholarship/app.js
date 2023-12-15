814a5d3e7c442eb572b2584fd97c4a84963dc1cd
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-15 17:11:12 +0200

    ex2

--- ExamPrep/02.Scholarship/app.js
+++ ExamPrep/02.Scholarship/app.js
@@ -1,7 +1,20 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    
-  
+  const name = document.getElementById('student');
+  const university = document.getElementById('university');
+  const score = document.getElementById('score');
+
+  document.getElementById("next-btn").addEventListener("click", next);
+
+  function next() {
+    if (name.value == "" ||
+      university.value == "" ||
+      score.value == "") {
+      return;
+    }
+
+    let previewList = document.getElementById("preview-list");
+    let publishedList = document.getElementById("candidates-list");
   }
-  
+}
