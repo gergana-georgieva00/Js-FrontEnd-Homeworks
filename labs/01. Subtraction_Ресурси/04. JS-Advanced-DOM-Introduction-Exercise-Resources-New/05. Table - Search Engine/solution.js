1ae5c3bd90c82401e891526907d5ce7ef9b01606
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-15 14:06:50 +0200

    ex5

--- labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const allCells = document.getElementsByTagName('tr');
+      const searchString = document.getElementById('searchField').value;
 
+      for (let index = 2; index < allCells.length; index++) {
+         const element = allCells[index];
+
+         if(element.textContent.includes(searchString)){
+            element.classList.add('select')
+         }
+      }
    }
 }
