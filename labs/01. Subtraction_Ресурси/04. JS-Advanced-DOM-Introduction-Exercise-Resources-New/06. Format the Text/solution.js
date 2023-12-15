d704fe32850d7d546e91aa429ef824f8da8d58ec
Gergana Georgieva <gerganagergana1@gmail.com>
2023-12-15 14:24:10 +0200

    ex6

--- labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ labs/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  const inputText = document.getElementById('input').value;
+  const sentences = inputText.split('.').filter(sentence => sentence.trim().length > 0);
+
+  const outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = ''; // Clear previous content
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    const paragraphText = sentences.slice(i, i + 3).join('. ') + '.'; // Add period after last sentence
+    const paragraph = `<p>${paragraphText}</p>`;
+    outputDiv.innerHTML += paragraph;
+  }
 }
